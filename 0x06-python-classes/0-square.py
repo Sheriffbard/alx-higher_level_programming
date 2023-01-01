010fe3e50155ce233d8d00694b0fd7179b0f3eb6
Sheriffbard <olawuyiola@gmail.com>
2023-01-01 14:37:28 -0800

    add

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-class Square():
-    '''
-        An empty square class
-    '''
+"""Write an empty class Square that defines a square:"""
+
+
+class Square:
+    """This is a comment"""
     pass
